7994030a0eaa2c17bcb71c0bd60a2209c86bfd51
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2024-08-28 12:09:19 -0500

    added env variables

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+import os
+from dotenv import load_dotenv
+
 from flask import Flask, request, render_template
 from google.oauth2 import service_account
 from google.cloud import documentai
 from google.api_core.client_options import ClientOptions
 from google.api_core.exceptions import GoogleAPIError
-import os
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/Users/Hrushikesh/Desktop/Trade_Sun_Project/OCR_GCP/service-account-key.json"
-
 import logging
+
+
+# LOADING ENVIRONMENT VARIABLES
+load_dotenv()
+GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+PROJECT_ID = os.environ.get("PROJECT_ID")
+LOCATION = os.environ.get("LOCATION")
+
 
 app = Flask(__name__)
 
@@ -14,16 +22,13 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
 def process_document_sample(file_content, processor_id, mime_type="application/pdf", field_mask="entities"):
-    project_id = "842588065610"
-    location = "us"
     
     try:
-        credentials = service_account.Credentials.from_service_account_file(
-            os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-        )
-        opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
+        credentials = service_account.Credentials.from_service_account_file(GOOGLE_APPLICATION_CREDENTIALS)
+
+        opts = ClientOptions(api_endpoint=f"{LOCATION}-documentai.googleapis.com")
         client = documentai.DocumentProcessorServiceClient(client_options=opts, credentials=credentials)
-        name = client.processor_path(project_id, location, processor_id)
+        name = client.processor_path(PROJECT_ID, LOCATION, processor_id)
 
         raw_document = documentai.RawDocument(content=file_content, mime_type=mime_type)
         request_obj = documentai.ProcessRequest(name=name, raw_document=raw_document, field_mask=field_mask)
